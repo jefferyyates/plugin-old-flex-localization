34ddf4d842086768543cf7a81509a89b2a028889
Jeff Yates <jeffery.yates@gmail.com>
2021-06-02 14:43:30 -0400

    updates for new .env format

--- src/FlexLocalizationPlugin.js
+++ src/FlexLocalizationPlugin.js
@@ -20,73 +20,75 @@ export default class FlexLocalizationPlugin extends FlexPlugin {
   init(flex, manager) {
     this.registerReducers(manager);
     // This approach works by determining the language the user would like (by Flex worker attributes,
-    // or browser setting, in this sample.  Use the method best suited to your use case.
-    // The language name/code is used as a file name for an HTTP get to retrieve a JSON file
+    // or browser setting, in this sample).  Use the method best suited to your use case.
+    // The language name/code is used as a file name for an HTTP GET to retrieve a JSON file
     // containing all the strings that Flex uses.  Make copies of the base file for each language
     // you might need.  Have them translated and store them with the original.
-    //
-    // If manager.workerClient.attributes.language evaluates, can use it to pick up some strings.
-    // THEN manager.strings should get replaced/updated/enhanced (use the ... operator!)
+  
+    // Set a default.
+    let myLanguage = "en-US";
 
-    // const myLanguage = manager.workerClient.attributes.language;
-    // To test, manually set myLanguage = "fr-CA";
-    // Better, go to Twilio Console, TaskRouter->WorkSpaces->(your workspace)->Workers,
-    // pick a worker, and add "language":"fr-CA" to the attributes.
-    // Or use browser settings.
-    // const myLanguage = navigator.language;
-    // Or manager data.
-    // const myLanguage = manager.configuration.language
+    // Could use browser data.
+    myLanguage = navigator.language || myLanguage;
+
     // Or flex data.
-    // const myLanguage = flex.defaultConfiguration.language
-    // What can I do with flex.languages ??
+    myLanguage = flex.defaultConfiguration.language || myLanguage;
 
-    const myLanguage = "fr-CA";
+    // Or manager data.  (This can be set in appConfig.js)
+    myLanguage = manager.configuration.language || myLanguage;
+
+    // Or manager.workerClient.attributes data.
+    myLanguage = manager.workerClient.attributes.language || myLanguage;
 
     const filePath = "/" + myLanguage + ".json";
+    console.log('FLEX_APP_FUNCTIONS_DOMAIN', process.env.FLEX_APP_FUNCTIONS_DOMAIN);
     console.log("filePath", filePath);
 
-    /*  async is problematic with race conditions 
-        - "init" finishes before manager.strings updates using this code
-    fetch(filePath)
-      .then(response => response.json())
-      .then(data => {
-      const newStrings = { ...manager.strings, ...data};
-      manager.strings = newStrings;
-      console.log("manager.strings.NoTasks", manager.strings.NoTasks);
-      console.log(data);
-    });
-    */
+    // Keep things synchronous to avoid race conditions
 
-    /*  sync call to http server
-    var request = new XMLHttpRequest();
-    request.open('GET', filePath, false);  // `false` makes the request synchronous
-    request.send(null);
-
-    if (request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      manager.strings = { ...manager.strings, ...data ;
-      console.log("manager.strings.NoTasks", manager.strings.NoTasks);
-    } else {
+    // First we show an HTTP request to fetch a file.
+    // Twilio serverless Services can expose Assets via URL, so
+    // for the purpose of this sample we will use that.
+    // The Asset MUST be marked "Public" for this to work!!!
+    try {
+      const request = new XMLHttpRequest();
+      request.open('GET', process.env.FLEX_APP_FUNCTIONS_DOMAIN + filePath, false);  // `false` makes the request synchronous
+      request.send(null);
+  
+      if (request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        manager.strings = { ...manager.strings, ...data };
+        console.log("manager.strings.NoTasks", manager.strings.NoTasks);
+      } else {
+        console.warn("translation file not found");
+      }  
+    }
+    catch(e) {
       console.warn("translation file not found");
     }
-    */
 
     // Could also use a Twilio serverless Service/Function/Assets
-    // Still sync mode!
-    const request = new XMLHttpRequest();
-    
-    request.open("POST", process.env.FLEX_PLUGIN_FUNCTIONS_DOMAIN + "/getStrings", false);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
-    const body = { 
-      languageRequested: filePath,
-      Token: manager.store.getState().flex.session.ssoTokenPayload.token
-    };
-    request.send(new URLSearchParams(body));
-    if(request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      manager.strings = { ...manager.strings, ...data};
-      console.log("manager.strings.NoTasks", manager.strings.NoTasks);
-    } else {
+    // Call a Function, which then creates or retrieves the translation data.
+    // The Asset MUST be marked "Private" for this to work!!!
+    try {
+      const request = new XMLHttpRequest();
+      
+      request.open("POST", process.env.FLEX_APP_FUNCTIONS_DOMAIN + "/getStrings", false);
+      request.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+      const body = { 
+        languageRequested: filePath,
+        Token: manager.store.getState().flex.session.ssoTokenPayload.token
+      };
+      request.send(new URLSearchParams(body));
+      if(request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        manager.strings = { ...manager.strings, ...data};
+        console.log("manager.strings.NoTasks", manager.strings.NoTasks);
+      } else {
+        console.warn("translation file not found");
+      }
+    }
+    catch(e) {
       console.warn("translation file not found");
     }
 
